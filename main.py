2b954be6d561b7b457e617e777ac319526ddc88b
MTito2 <mateuscunha166@gmail.com>
2025-07-29 13:42:13 -0300

    Alterações gerais

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bot import bot
 from modules.diet_module import handler_diet
+from handlers import general_handlers
 
 if __name__ == "__main__":
     print("Bot iniciado")
--- modules/diet_module/handler_diet.py
+++ modules/diet_module/handler_diet.py
@@ -7,7 +7,7 @@ sys.path.insert(0, str(ROOT_DIR))
 from bot import bot
 from modules.diet_module.ia_diet import receive_message
 
-@bot.message_handler(commands=["sugerir_cardapio"])
+@bot.message_handler(commands=["sugerir_refeicao"])
 def send_diet(message):
     response_ia = receive_message()
     bot.reply_to(message, response_ia)
